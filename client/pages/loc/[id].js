e942121b5e94c974c1e49fd1e9204f7d6d715ff7
hoangle4 <hoang.le4@pcc.edu>
2020-01-01 18:47:54 -0800

    commit redirect

--- client/pages/loc/[id].js
+++ client/pages/loc/[id].js
@@ -1,43 +1,47 @@
-import {connect} from 'react-redux';
-import {getLocation} from '../../action/location';
+import { connect } from 'react-redux';
+import { getLocation } from '../../action/location';
 import fetch from 'isomorphic-unfetch';
-
+import Router from 'next/router';
 
 const Loc = ({
-  id,
-  businessName,
-  address,
-  city,
-  state,
-  zipcode,
-  phoneNumber,
-  email,
-  avatar,
-  admin,
-  createdAt,
-  updatedAt
+	id,
+	businessName,
+	address,
+	city,
+	state,
+	zipcode,
+	phoneNumber,
+	email,
+	avatar,
+	admin,
+	createdAt,
+	updatedAt
 }) => {
-    return (
-      <div> Location </div>
-    );
+	return <div> Location </div>;
 };
 
-Loc.getInitialProps = async (context) => {
-    const {id} = context.query;
-    const token = localStorage.getItem('token');
-    const res = await fetch(`http://localhost:3001/api/location/${id}`,
-      {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            't-auth-token':token
-        }
-      });
-      const result = await res.json();
-    console.log(result);
+Loc.getInitialProps = async ({ res, query, isServer }) => {
+	if (isServer) {
+		res.writeHead(302, {
+			Location: '/'
+		});
+		res.end();
+	}
 
-    return {result};
-  };
+	const { id } = query;
+	const token = localStorage.getItem('token');
+	const response = await fetch(`http://localhost:3001/api/location/${id}`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json',
+			't-auth-token': token
+		}
+	});
+	const result = await response.json();
+	console.log(result);
 
-export default connect(null, {getLocation})(Loc);
+	return { result };
+};
+
+export default connect(null, { getLocation })(Loc);
